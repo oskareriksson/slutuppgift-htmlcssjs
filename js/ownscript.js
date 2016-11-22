8e9b36cdeccf608fcaae2576bfc299c936cddef3
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2016-11-22 15:16:31 +0100

    Added JS function that adds a user tip to the DOM on button click

--- js/ownscript.js
+++ js/ownscript.js
@@ -1,1 +1,14 @@
 // My own JavaScript
+
+var tip = document.createElement("p");
+var txt = document.createTextNode("Psst! You can also find Alan on various social media.")
+tip.appendChild(txt);
+
+var submit = document.querySelector("button");
+submit.addEventListener("click", socialMedia);
+
+function socialMedia() {
+
+	var ptag = document.getElementById("success");
+	ptag.parentNode.insertBefore(tip, ptag.nextSibling);
+};
