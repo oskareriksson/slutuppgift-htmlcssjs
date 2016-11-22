9607a321c8e1a14fff84c0232e69216bf5cf704a
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2016-11-22 17:00:55 +0100

    Added styling to the vanilla JS element

--- js/ownscript.js
+++ js/ownscript.js
@@ -1,6 +1,11 @@
 // My own JavaScript
 
 var tip = document.createElement("p");
+tip.style.backgroundColor = "#c99a76";
+tip.style.textAlign = "center";
+tip.style.margin = "auto";
+tip.style.width = "60%";
+
 var txt = document.createTextNode("Psst! You can also find Alan on various social media.")
 tip.appendChild(txt);
 
